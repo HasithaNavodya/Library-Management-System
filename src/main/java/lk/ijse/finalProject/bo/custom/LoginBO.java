7e946c50f8eb74e005ad0c0bfea80c53a70514da
HasithaNavodya <hasithanavodya@gmail.com>
2023-06-11 14:12:48 +0530

    last

--- src/main/java/lk/ijse/finalProject/bo/custom/LoginBO.java
+++ src/main/java/lk/ijse/finalProject/bo/custom/LoginBO.java
@@ -5,7 +5,7 @@ import lk.ijse.finalProject.dto.UserDTO;
 
 import java.sql.SQLException;
 
-public interface LoginBO  {
+public interface LoginBO extends SuperBO {
 
     public  boolean LoginAction(UserDTO userDTO) throws SQLException;
 }
